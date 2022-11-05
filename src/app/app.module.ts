2d07e9744439635cf2f5d095e0c44e30e94fdda2
BuildTools <unconfigured@null.spigotmc.org>
2022-11-05 18:42:10 +0200

    ltvk uzduotis

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CartComponent } from './components/cart/cart.component';
+import { AddProductComponent } from './components/add-product/add-product.component';
+import { FormsModule } from '@angular/forms';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { Routes,RouterModule } from '@angular/router';
+import { EditProductComponent } from './components/edit-product/edit-product.component';
+import { CartSummaryComponent } from './components/cart-summary/cart-summary.component';
+
+
+const appRoutes:Routes=[
+  {path:'',    component:CartComponent},
+  {path:'new',component:AddProductComponent},
+  {path:'edit/:id',component:EditProductComponent}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CartComponent,
+    AddProductComponent,
+    NavigationComponent,
+    EditProductComponent,
+    CartSummaryComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
